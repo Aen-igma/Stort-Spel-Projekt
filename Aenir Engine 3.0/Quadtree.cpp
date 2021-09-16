20b1bd8b8b65b5204a6618395f3a26acda721497
Linus Dorell <81676011+NoteFez@users.noreply.github.com>
2021-09-16 16:31:57 +0200

    Work on quad class

--- Aenir Engine 3.0/Quadtree.cpp
+++ Aenir Engine 3.0/Quadtree.cpp
@@ -4,7 +4,7 @@
 Quadtree::Quadtree()
 {
 	m_topLeft = Point(0,0);
-	m_topRight = Point(0, 0);
+	m_botRight = Point(0, 0);
 	pnode = NULL;
 	m_ptopLeftTree = NULL;
 	m_ptopRightTree = NULL;
@@ -20,7 +20,7 @@ Quadtree::Quadtree(Point topL, Point botR)
 	m_pbotLeftTree = NULL;
 	m_pbotLeftTree = NULL;
 	m_topLeft = topL;
-	m_topRight = botR;
+	m_botRight = botR;
 }
 
 Quadtree::~Quadtree()
@@ -38,6 +38,36 @@ void Quadtree::insert(Node* node)
 	if (!inBoundery(node->m_pos))
 	{
 		return;
+	}
+	
+	if (abs(m_topLeft.m_x - m_botRight.m_y ) <= 1 && 
+		abs(m_topLeft.m_y - m_botRight.m_y) <= 1)
+	{
+		if (pnode = NULL)
+		{
+			pnode = node;
+		}
+		return;
+	}
+	if ((m_topLeft.m_x + m_botRight.m_x) / 2 >= node->m_pos.m_x)
+	{
+		if ((m_topLeft.m_y + m_botRight.m_y) / 2 >= node->m_pos.m_y)
+		{
+			if (m_ptopLeftTree == NULL)
+			{
+				m_ptopLeftTree = new Quadtree(Point(m_topLeft.m_x, m_botRight.m_y),
+					Point((m_topLeft.m_x, m_botRight.m_y) /2, (m_topLeft.m_y + m_botRight.m_y) / 2));
+				m_ptopLeftTree->insert(node);
+			}
+		}
+		else
+		{
+			if (m_pbotLeftTree == NULL)
+			{
+
+			}
+		}
+
 	}
 }
 
