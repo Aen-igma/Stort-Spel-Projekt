545cf6463c3cc717e5e0e84bcdf5f3351b431664
Linus Dorell <81676011+NoteFez@users.noreply.github.com>
2021-09-16 15:41:18 +0200

    Quadtree Class work.

--- Aenir Engine 3.0/Quadtree.cpp
+++ Aenir Engine 3.0/Quadtree.cpp
@@ -3,8 +3,50 @@
 
 Quadtree::Quadtree()
 {
+	m_topLeft = Point(0,0);
+	m_topRight = Point(0, 0);
+	pnode = NULL;
+	m_ptopLeftTree = NULL;
+	m_ptopRightTree = NULL;
+	m_pbotLeftTree = NULL;
+	m_pbotLeftTree = NULL;
+}
+
+Quadtree::Quadtree(Point topL, Point botR)
+{
+	pnode = NULL;
+	m_ptopLeftTree = NULL;
+	m_ptopRightTree = NULL;
+	m_pbotLeftTree = NULL;
+	m_pbotLeftTree = NULL;
+	m_topLeft = topL;
+	m_topRight = botR;
 }
 
 Quadtree::~Quadtree()
 {
+}
+
+//Node into quadtree
+void Quadtree::insert(Node* node)
+{
+	if (node == NULL)
+	{
+		return;
+	}
+	//quad cant contain 
+	if (!inBoundery(node->m_pos))
+	{
+		return;
+	}
+}
+
+Node* Quadtree::search(Point point)
+{
+	return nullptr;
+}
+
+bool Quadtree::inBoundery(Point)
+{
+	return false;
 }
--- Aenir Engine 3.0/Quadtree.h
+++ Aenir Engine 3.0/Quadtree.h
@@ -5,27 +5,49 @@
 #include "Graphics/Graphics.h"
 
 //Points where we mark on the 2D space
-struct point {
+struct Point {
 	float m_x;
 	float m_y;
-	point(float x, float y) {
+	Point(float x, float y) {
 		this->m_x = x;
 		this->m_y = y;
+	}
+	Point()
+	{
+		m_x = 0;
+		m_y = 0;
 	}
 };
 
 //To construct the boundery box, needs to use the window x,y for right cordinates
-struct rectangle
+struct Rectangle
 {
 	float m_x;
 	float m_y;
 	float m_z;
 	float m_h;
-	rectangle(float  x, float y, float z, float h) {
+	Rectangle(float  x, float y, float z, float h) {
 		this->m_x = x;
 		this->m_y = y;
 		this->m_z = z;
 		this->m_h = h;
+	}
+
+};
+
+//The objects that we wanted stord in quadtree
+struct Node
+{
+	Point m_pos;
+	int m_data;
+	Node(Point pos, int data)
+	{
+		m_pos = pos;
+		m_data = data;
+	}
+	Node()
+	{
+		m_data = 0;
 	}
 
 };
@@ -35,13 +57,28 @@ class Quadtree
 {
 public:
 	Quadtree();
+	Quadtree(Point topL, Point botR);
 	~Quadtree();
+
+	void insert(Node* node);
+	Node* search(Point point);
+	bool inBoundery(Point);
 
 	void boundery(float x, float y, float z, float h){};
 	void subDivide() {};
 private:
-	int capacity;
 
+	Point m_topLeft;
+	Point m_topRight;
+
+	Node* pnode;
+
+	Quadtree *m_ptopLeftTree;
+	Quadtree *m_ptopRightTree;
+	Quadtree* m_pbotLeftTree;
+	Quadtree* m_pbotRightTree;
+
+	int capacity;
 	float m_x;
 	float m_y;
 	float m_z;
