315531418f1e67c6dd3fa7b67ecdcb1f1e0c4a8a
ante <49555217+ante185@users.noreply.github.com>
2021-09-23 15:06:14 +0200

    Update CreateThumbnail.h
    additional functions

--- Aenir Engine 3.0/CreateThumbnail.h
+++ Aenir Engine 3.0/CreateThumbnail.h
@@ -16,6 +16,12 @@ struct ImageByteData {
 	static void MakeThumbnail(const ImageByteData* source, const ImageByteData* destination) {
 		stbir_resize_uint8(source->imageData.get(), source->x, source->y, 1, destination->imageData.get(), destination->x, destination->y, 1, destination->channels);
 	}
+	void MakeThumbnail(const ImageByteData* destination) {
+		stbir_resize_uint8(this->imageData.get(), this->x, this->y, 1, destination->imageData.get(), destination->x, destination->y, 1, destination->channels);
+	}
+	void SaveImage(const char* filepath) {
+		stbi_write_png(filepath, this->x, this->y, 8, this->imageData.get(), 1);
+	}
 	static void SaveImage(const ImageByteData* source, const char* filepath) {
 		stbi_write_png(filepath, source->x, source->y, 8, source->imageData.get(), 1);
 	}
