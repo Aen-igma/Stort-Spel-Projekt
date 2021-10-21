640f869c1be365ee27e5b07fe7868d3ea6dcb4be
15juga <81560600+15juga@users.noreply.github.com>
2021-10-21 16:43:55 +0200

    Moved Initialize Up

--- Client/src/States/Gameplay.cpp
+++ Client/src/States/Gameplay.cpp
@@ -35,6 +35,175 @@ Gameplay::~Gameplay()
 	m_ReimuTex = nullptr;
 }
 
+void Gameplay::Initialize()
+{
+	State::SetLoad(false);
+	char q = 219;
+	// ----------------------------- Setup Camera ------------------------------- //
+
+	m_camera = &Aen::EntityHandler::CreateEntity();
+	m_camera->AddComponent<Aen::Camera>();
+	m_camera->GetComponent<Aen::Camera>().SetCameraPerspective(70.f, m_Window.GetAspectRatio(), 0.01f, 100.f);
+	m_camera->SetPos(0.f, 2.f, -2.f);
+
+	Aen::GlobalSettings::SetMainCamera(*m_camera);
+
+	// ------------------------ Setup Directional Light ------------------------- //
+
+	m_dLight = &Aen::EntityHandler::CreateEntity();
+	m_dLight->AddComponent<Aen::DirectionalLight>();
+	m_dLight->GetComponent<Aen::DirectionalLight>().SetColor(Aen::Color::White);
+	m_dLight->GetComponent<Aen::DirectionalLight>().SetStrength(1.f);
+	m_dLight->SetRot(45.f, -135.f, 0.f);
+
+	// --------------------------- Setup Spot Light ----------------------------- //
+
+	m_spotLight = &Aen::EntityHandler::CreateEntity();
+	m_spotLight->AddComponent<Aen::SpotLight>();
+	m_spotLight->GetComponent<Aen::SpotLight>().SetColor(Aen::Color::Red);
+	m_spotLight->GetComponent<Aen::SpotLight>().SetStrength(1.f);
+	m_spotLight->GetComponent<Aen::SpotLight>().SetConeSize(40.f);
+	m_spotLight->GetComponent<Aen::SpotLight>().SetStrength(500.f);
+	m_spotLight->GetComponent<Aen::SpotLight>().SetLightDist(1.f, 0.f, 0.f, 10.f);
+	m_spotLight->SetPos(0.f, 2.f, -5.f);
+	m_spotLight->SetRot(45.f, 0.f, 0.f);
+
+	// ----------------------------- Load Meshes -------------------------------- //
+
+	Aen::Mesh& plane = Aen::Resource::CreateMesh("Plane");
+	Aen::Mesh& skele = Aen::Resource::CreateMesh("Cylinder");
+	Aen::Mesh& gob = Aen::Resource::CreateMesh("Goblin");
+	Aen::Mesh& cube = Aen::Resource::CreateMesh("Cube");
+	Aen::Mesh& sphere = Aen::Resource::CreateMesh("Sphere");
+	plane.Load(AEN_RESOURCE_DIR("Plane.obj"));
+	skele.Load(AEN_RESOURCE_DIR("combinedShapes.fbx"));
+	gob.Load(AEN_RESOURCE_DIR("GobTriMtrl.fbx"));
+	//skele.PrintMaterialSlots();
+	cube.Load(AEN_RESOURCE_DIR("Cube.obj"));
+	sphere.Load(AEN_RESOURCE_DIR("Sphere.obj"));
+
+	// ----------------------------- Load Reimushes -------------------------------- //
+
+	m_ReimuTex = &Aen::Resource::CreateTexture("ReimuTex");
+	m_ReimuMat = &Aen::Resource::CreateMaterial("ReimuMat");
+	m_reimubeMesh = &Aen::Resource::CreateMesh("Cube");
+	m_reimubeMesh->Load(AEN_RESOURCE_DIR("Cube.obj"));
+	m_ReimuTex->LoadTexture(AEN_RESOURCE_DIR("Reimu.png"));
+	m_ReimuMat->SetDiffuseMap(*m_ReimuTex);
+
+	(*m_ReimuMat)["OuterEdgeColor"] = Aen::Color(0.9f, 0.33f, 0.5f, 1.f);
+	(*m_ReimuMat)["InnerEdgeColor"] = Aen::Color(0.9f, 0.33f, 0.5f, 1.f);
+	(*m_ReimuMat)["OuterEdgeThickness"] = 3;
+	(*m_ReimuMat)["InnerEdgeThickness"] = 3;
+
+	m_meshcube = &cube;
+
+	// ASSIMP
+	Aen::Texture& pengDiff = Aen::Resource::CreateTexture("PengTexture");
+	Aen::Material& pengMtrl = Aen::Resource::CreateMaterial("PengMtrl");
+	pengDiff.LoadTexture(AEN_RESOURCE_DIR("gunter2.png"));
+	pengMtrl.SetDiffuseMap(pengDiff);
+
+	Aen::Texture& gobDiff = Aen::Resource::CreateTexture("GobTexture");
+	Aen::Material& gobMtrl = Aen::Resource::CreateMaterial("GobMtrl");
+	gobDiff.LoadTexture(AEN_RESOURCE_DIR("body_diffuse.png"));
+	gobMtrl.SetDiffuseMap(gobDiff);
+	// -------------------------- Setup Entities -------------------------------- //
+
+	m_sphere = &Aen::EntityHandler::CreateEntity();
+	m_sphere->AddComponent<Aen::MeshInstance>();
+	m_sphere->GetComponent<Aen::MeshInstance>().SetMesh(sphere);
+
+	m_plane = &Aen::EntityHandler::CreateEntity();
+	m_plane->AddComponent<Aen::MeshInstance>();
+	m_plane->GetComponent<Aen::MeshInstance>().SetMesh(plane);
+	m_plane->AddComponent<Aen::RigidBody>();
+	m_plane->GetComponent<Aen::RigidBody>().CreateMaterial();
+	m_plane->GetComponent<Aen::RigidBody>().CreatePlane();
+
+	//m_plane.SetPos(0.f, -2.f, 0.f);
+	m_plane->SetScale(40.f, 1.f, 40.f);
+
+	/*m_plane1.AddComponent<Aen::MeshInstance>();
+	m_plane1.GetComponent<Aen::MeshInstance>().SetMesh(plane);
+	m_plane1.SetPos(0.f, 8.f, -10.f);
+	m_plane1.SetScale(20.f, 1.f, 20.f);
+	m_plane1.SetRot(90.f, 0.f, 0.f);*/
+
+	m_cube = &Aen::EntityHandler::CreateEntity();
+	m_cube->AddComponent<Aen::MeshInstance>();
+	m_cube->GetComponent<Aen::MeshInstance>().SetMesh(cube);
+	m_cube->SetPos(0.f, 8.f, 10.f);
+	m_cube->SetScale(20.f, 20.f, 1.f);
+	m_cube->SetRenderLayer(-1);
+
+	m_cube = &Aen::EntityHandler::CreateEntity();
+	m_cube->AddComponent<Aen::MeshInstance>();
+	m_cube->GetComponent<Aen::MeshInstance>().SetMesh(cube);
+	m_cube->SetPos(0.f, 8.f, 10.f);
+	m_cube->SetScale(20.f, 20.f, 1.f);
+
+	m_goblin = &Aen::EntityHandler::CreateEntity();
+	m_goblin->AddComponent<Aen::MeshInstance>();
+	m_goblin->GetComponent<Aen::MeshInstance>().SetMesh(gob);
+	m_goblin->GetComponent<Aen::MeshInstance>().SetMaterial(gobMtrl);
+
+	m_goblin->SetPos(0.f, 2.f, 4.f);
+
+	/* ----------------------------- Load EmissionCube -------------------------------- */
+
+	m_emiCube = &Aen::EntityHandler::CreateEntity();
+	m_emiCube->AddComponent<Aen::MeshInstance>();
+	m_emiCube->GetComponent<Aen::MeshInstance>().SetMesh(cube);
+	m_emiCube->SetPos(0.f, 3.f, -5.f);
+
+
+	Aen::Texture& face = Aen::Resource::CreateTexture("FaceTexture");
+	Aen::Texture& peng = Aen::Resource::CreateTexture("NekoTexture");
+	Aen::Material& emissionMtrl = Aen::Resource::CreateMaterial("EmiMtrl");
+	face.LoadTexture(AEN_RESOURCE_DIR("emmissionS.png"));
+	peng.LoadTexture(AEN_RESOURCE_DIR("gunter2.png"));
+	emissionMtrl.SetEmissionMap(face);
+	emissionMtrl.SetDiffuseMap(peng);
+	emissionMtrl["GlowColor"] = Aen::Color::Green;
+	emissionMtrl["OuterEdgeThickness"] = 2;
+	emissionMtrl["InnerEdgeThickness"] = 2;
+	emissionMtrl["OuterEdgeColor"] = Aen::Color::Green;
+	emissionMtrl["InnerEdgeColor"] = Aen::Color::Green;
+
+	m_emiCube->GetComponent<Aen::MeshInstance>().SetMaterial(emissionMtrl);
+	// --------------------------- Setup Window --------------------------------- //
+
+	m_Window.SetWindowSize(static_cast<UINT>(GetSystemMetrics(SM_CXSCREEN) * 0.4f), static_cast<UINT>(GetSystemMetrics(SM_CYSCREEN) * 0.4f));
+
+	// ------------------- Procedural generation testing staging grounds ------- //
+
+	//LevelGenerator::GenerationTestingFunction();
+	/*srand((unsigned int)time(NULL));
+	SetLehmerConstSeed(100);
+	LehmerInt();
+	Room* map = LevelGenerator::GenerationTestingFunction();*/
+
+	/*for (UINT y = 0; y < mapSize; y++) {
+		for (UINT x = 0; x < mapSize; x++) {
+			if (map[x + y * mapSize].m_present) {
+				rooms[x + y * mapSize] = new Aen::Entity();
+				rooms[x + y * mapSize]->AddComponent<Aen::MeshInstance>();
+				rooms[x + y * mapSize]->GetComponent<Aen::MeshInstance>().SetMesh(*m_meshcube);
+				rooms[x + y * mapSize]->SetPos(x * 2, 0.f, y * 2);
+			}
+			else {
+				if (rooms[x + y * mapSize] != nullptr)
+					delete rooms[x + y * mapSize];
+				rooms[x + y * mapSize] = nullptr;
+			}
+		}
+	}*/
+
+	Aen::Input::ToggleRawMouse(false);
+	cout << "Press Enter To Continue\n";
+}
+
 void Gameplay::Update(const float& deltaTime)
 {
 	// --------------------------- Raw Mouse and scroll Input --------------------------- //
@@ -210,173 +379,3 @@ void Gameplay::Update(const float& deltaTime)
 
 	//cout << m_camera.GetRot().y << endl;
 }
-
-void Gameplay::Initialize()
-{
-	State::SetLoad(false);
-	char q = 219;
-	// ----------------------------- Setup Camera ------------------------------- //
-
-	m_camera = &Aen::EntityHandler::CreateEntity();
-	m_camera->AddComponent<Aen::Camera>();
-	m_camera->GetComponent<Aen::Camera>().SetCameraPerspective(70.f, m_Window.GetAspectRatio(), 0.01f, 100.f);
-	m_camera->SetPos(0.f, 2.f, -2.f);
-
-	Aen::GlobalSettings::SetMainCamera(*m_camera);
-
-	// ------------------------ Setup Directional Light ------------------------- //
-
-	m_dLight = &Aen::EntityHandler::CreateEntity();
-	m_dLight->AddComponent<Aen::DirectionalLight>();
-	m_dLight->GetComponent<Aen::DirectionalLight>().SetColor(Aen::Color::White);
-	m_dLight->GetComponent<Aen::DirectionalLight>().SetStrength(1.f);
-	m_dLight->SetRot(45.f, -135.f, 0.f);
-
-	// --------------------------- Setup Spot Light ----------------------------- //
-
-	m_spotLight = &Aen::EntityHandler::CreateEntity();
-	m_spotLight->AddComponent<Aen::SpotLight>();
-	m_spotLight->GetComponent<Aen::SpotLight>().SetColor(Aen::Color::Red);
-	m_spotLight->GetComponent<Aen::SpotLight>().SetStrength(1.f);
-	m_spotLight->GetComponent<Aen::SpotLight>().SetConeSize(40.f);
-	m_spotLight->GetComponent<Aen::SpotLight>().SetStrength(500.f);
-	m_spotLight->GetComponent<Aen::SpotLight>().SetLightDist(1.f, 0.f, 0.f, 10.f);
-	m_spotLight->SetPos(0.f, 2.f, -5.f);
-	m_spotLight->SetRot(45.f, 0.f, 0.f);
-
-	// ----------------------------- Load Meshes -------------------------------- //
-
-	Aen::Mesh& plane = Aen::Resource::CreateMesh("Plane");
-	Aen::Mesh& skele = Aen::Resource::CreateMesh("Cylinder");
-	Aen::Mesh& gob = Aen::Resource::CreateMesh("Goblin");
-	Aen::Mesh& cube = Aen::Resource::CreateMesh("Cube");
-	Aen::Mesh& sphere = Aen::Resource::CreateMesh("Sphere");
-	plane.Load(AEN_RESOURCE_DIR("Plane.obj"));
-	skele.Load(AEN_RESOURCE_DIR("combinedShapes.fbx"));
-	gob.Load(AEN_RESOURCE_DIR("GobTriMtrl.fbx"));
-	//skele.PrintMaterialSlots();
-	cube.Load(AEN_RESOURCE_DIR("Cube.obj"));
-	sphere.Load(AEN_RESOURCE_DIR("Sphere.obj"));
-
-	// ----------------------------- Load Reimushes -------------------------------- //
-
-	m_ReimuTex = &Aen::Resource::CreateTexture("ReimuTex");
-	m_ReimuMat = &Aen::Resource::CreateMaterial("ReimuMat");
-	m_reimubeMesh = &Aen::Resource::CreateMesh("Cube");
-	m_reimubeMesh->Load(AEN_RESOURCE_DIR("Cube.obj"));
-	m_ReimuTex->LoadTexture(AEN_RESOURCE_DIR("Reimu.png"));
-	m_ReimuMat->SetDiffuseMap(*m_ReimuTex);
-
-	(*m_ReimuMat)["OuterEdgeColor"] = Aen::Color(0.9f, 0.33f, 0.5f, 1.f);
-	(*m_ReimuMat)["InnerEdgeColor"] = Aen::Color(0.9f, 0.33f, 0.5f, 1.f);
-	(*m_ReimuMat)["OuterEdgeThickness"] = 3;
-	(*m_ReimuMat)["InnerEdgeThickness"] = 3;
-
-	m_meshcube = &cube;
-
-	// ASSIMP
-	Aen::Texture& pengDiff = Aen::Resource::CreateTexture("PengTexture");
-	Aen::Material& pengMtrl = Aen::Resource::CreateMaterial("PengMtrl");
-	pengDiff.LoadTexture(AEN_RESOURCE_DIR("gunter2.png"));
-	pengMtrl.SetDiffuseMap(pengDiff);
-
-	Aen::Texture& gobDiff = Aen::Resource::CreateTexture("GobTexture");
-	Aen::Material& gobMtrl = Aen::Resource::CreateMaterial("GobMtrl");
-	gobDiff.LoadTexture(AEN_RESOURCE_DIR("body_diffuse.png"));
-	gobMtrl.SetDiffuseMap(gobDiff);
-	// -------------------------- Setup Entities -------------------------------- //
-
-	m_sphere = &Aen::EntityHandler::CreateEntity();
-	m_sphere->AddComponent<Aen::MeshInstance>();
-	m_sphere->GetComponent<Aen::MeshInstance>().SetMesh(sphere);
-
-	m_plane = &Aen::EntityHandler::CreateEntity();
-	m_plane->AddComponent<Aen::MeshInstance>();
-	m_plane->GetComponent<Aen::MeshInstance>().SetMesh(plane);
-	m_plane->AddComponent<Aen::RigidBody>();
-	m_plane->GetComponent<Aen::RigidBody>().CreateMaterial();
-	m_plane->GetComponent<Aen::RigidBody>().CreatePlane();
-
-	//m_plane.SetPos(0.f, -2.f, 0.f);
-	m_plane->SetScale(40.f, 1.f, 40.f);
-
-	/*m_plane1.AddComponent<Aen::MeshInstance>();
-	m_plane1.GetComponent<Aen::MeshInstance>().SetMesh(plane);
-	m_plane1.SetPos(0.f, 8.f, -10.f);
-	m_plane1.SetScale(20.f, 1.f, 20.f);
-	m_plane1.SetRot(90.f, 0.f, 0.f);*/
-
-	m_cube = &Aen::EntityHandler::CreateEntity();
-	m_cube->AddComponent<Aen::MeshInstance>();
-	m_cube->GetComponent<Aen::MeshInstance>().SetMesh(cube);
-	m_cube->SetPos(0.f, 8.f, 10.f);
-	m_cube->SetScale(20.f, 20.f, 1.f);
-	m_cube->SetRenderLayer(-1);
-
-	m_cube = &Aen::EntityHandler::CreateEntity();
-	m_cube->AddComponent<Aen::MeshInstance>();
-	m_cube->GetComponent<Aen::MeshInstance>().SetMesh(cube);
-	m_cube->SetPos(0.f, 8.f, 10.f);
-	m_cube->SetScale(20.f, 20.f, 1.f);
-
-	m_goblin = &Aen::EntityHandler::CreateEntity();
-	m_goblin->AddComponent<Aen::MeshInstance>();
-	m_goblin->GetComponent<Aen::MeshInstance>().SetMesh(gob);
-	m_goblin->GetComponent<Aen::MeshInstance>().SetMaterial(gobMtrl);
-
-	m_goblin->SetPos(0.f, 2.f, 4.f);
-
-	/* ----------------------------- Load EmissionCube -------------------------------- */
-
-	m_emiCube = &Aen::EntityHandler::CreateEntity();
-	m_emiCube->AddComponent<Aen::MeshInstance>();
-	m_emiCube->GetComponent<Aen::MeshInstance>().SetMesh(cube);
-	m_emiCube->SetPos(0.f, 3.f, -5.f);
-
-
-	Aen::Texture& face = Aen::Resource::CreateTexture("FaceTexture");
-	Aen::Texture& peng = Aen::Resource::CreateTexture("NekoTexture");
-	Aen::Material& emissionMtrl = Aen::Resource::CreateMaterial("EmiMtrl");
-	face.LoadTexture(AEN_RESOURCE_DIR("emmissionS.png"));
-	peng.LoadTexture(AEN_RESOURCE_DIR("gunter2.png"));
-	emissionMtrl.SetEmissionMap(face);
-	emissionMtrl.SetDiffuseMap(peng);
-	emissionMtrl["GlowColor"] = Aen::Color::Green;
-	emissionMtrl["OuterEdgeThickness"] = 2;
-	emissionMtrl["InnerEdgeThickness"] = 2;
-	emissionMtrl["OuterEdgeColor"] = Aen::Color::Green;
-	emissionMtrl["InnerEdgeColor"] = Aen::Color::Green;
-
-	m_emiCube->GetComponent<Aen::MeshInstance>().SetMaterial(emissionMtrl);
-	// --------------------------- Setup Window --------------------------------- //
-
-	m_Window.SetWindowSize(static_cast<UINT>(GetSystemMetrics(SM_CXSCREEN) * 0.4f), static_cast<UINT>(GetSystemMetrics(SM_CYSCREEN) * 0.4f));
-
-	// ------------------- Procedural generation testing staging grounds ------- //
-
-	//LevelGenerator::GenerationTestingFunction();
-	/*srand((unsigned int)time(NULL));
-	SetLehmerConstSeed(100);
-	LehmerInt();
-	Room* map = LevelGenerator::GenerationTestingFunction();*/
-
-	/*for (UINT y = 0; y < mapSize; y++) {
-		for (UINT x = 0; x < mapSize; x++) {
-			if (map[x + y * mapSize].m_present) {
-				rooms[x + y * mapSize] = new Aen::Entity();
-				rooms[x + y * mapSize]->AddComponent<Aen::MeshInstance>();
-				rooms[x + y * mapSize]->GetComponent<Aen::MeshInstance>().SetMesh(*m_meshcube);
-				rooms[x + y * mapSize]->SetPos(x * 2, 0.f, y * 2);
-			}
-			else {
-				if (rooms[x + y * mapSize] != nullptr)
-					delete rooms[x + y * mapSize];
-				rooms[x + y * mapSize] = nullptr;
-			}
-		}
-	}*/
-
-	Aen::Input::ToggleRawMouse(false);
-	cout << "Press Enter To Continue\n";
-}
-
