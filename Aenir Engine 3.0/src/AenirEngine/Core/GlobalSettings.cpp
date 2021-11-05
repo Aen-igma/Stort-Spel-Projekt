2bb4aa177a48ea91398dc04a0e839b0d5294a47f
15juga <81560600+15juga@users.noreply.github.com>
2021-11-05 14:54:17 +0100

    Update GlobalSettings.cpp

--- Aenir Engine 3.0/src/AenirEngine/Core/GlobalSettings.cpp
+++ Aenir Engine 3.0/src/AenirEngine/Core/GlobalSettings.cpp
@@ -8,4 +8,7 @@ namespace Aen {
 	Entity* GlobalSettings::m_pMainCamera(nullptr);
 	Color GlobalSettings::m_BGColor(0.08f, 0.08f, 0.13f, 1.f);
 	//ImGuiHandler* GlobalSettings::mp_guiHandler = AEN_NEW ImGuiHandler();
+
+	Material* GlobalSettings::m_defaultMaterial(nullptr);
+	Texture* GlobalSettings::m_defaultTexture(nullptr);
 }
