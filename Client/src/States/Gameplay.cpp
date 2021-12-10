a68fd4b6433e00e17864e56d67616c94ad43637a
15juga <81560600+15juga@users.noreply.github.com>
2021-12-10 11:59:36 +0100

    fixed portal again

--- Client/src/States/Gameplay.cpp
+++ Client/src/States/Gameplay.cpp
@@ -68,6 +68,8 @@ void Gameplay::Initialize()
 	skeleLight.Load(AEN_MODEL_DIR("Skel_Light_Walking_2.fbx"));
 	Aen::Mesh& throne = Aen::Resource::CreateMesh("Throne");
 	throne.Load(AEN_MODEL_DIR("Throne.fbx"));
+	Aen::Mesh& plane = Aen::Resource::CreateMesh("Flat");
+	plane.Load(AEN_MODEL_DIR("flat.fbx"));
 
 	// -------------------------- Setup Material -------------------------------- //
 
@@ -240,7 +242,7 @@ void Gameplay::Initialize()
 		m_pSkeleBoss->SetThronePosition(m_throne->GetPos().x - 1.f, m_throne->GetPos().y, m_throne->GetPos().z);
 		m_pSkeleBoss->GetEntity()->SetRot(0.f, 0.f, 0.f);
 		m_grave.GetEntity()->SetRot(0, 180, 0);
-		m_exit.GetEntity()->SetRot(-90, -90, 90);
+		m_exit.GetEntity()->SetRot(0, 180, 0);
 	}
 	else if (roomNormal == 10) {//east
 		m_throne->SetPos(m_bossPos.x - 33.35f, m_bossPos.y + 6.11f, m_bossPos.z);
@@ -249,7 +251,7 @@ void Gameplay::Initialize()
 		m_pSkeleBoss->SetThronePosition(m_throne->GetPos().x, m_throne->GetPos().y, m_throne->GetPos().z - 1.f);
 		m_pSkeleBoss->GetEntity()->SetRot(0.f, -90.f, 0.f);
 		m_grave.GetEntity()->SetRot(0, 90, 0);
-		m_exit.GetEntity()->SetRot(90, 180, 90);
+		m_exit.GetEntity()->SetRot(0, 90, 0);
 	}
 	else if (roomNormal == 100) {//south
 		m_throne->SetPos(m_bossPos.x, m_bossPos.y + 6.11f, m_bossPos.z - 33.35f);
@@ -258,7 +260,7 @@ void Gameplay::Initialize()
 		m_pSkeleBoss->SetThronePosition(m_throne->GetPos().x +1.f, m_throne->GetPos().y, m_throne->GetPos().z);
 		m_pSkeleBoss->GetEntity()->SetRot(0.f, 180.f, 0.f);
 		m_grave.GetEntity()->SetRot(0, 0, 0);
-		m_exit.GetEntity()->SetRot(-90, 90, 90);
+		m_exit.GetEntity()->SetRot(0, 0, 0);
 	}
 	else if (roomNormal == 1000) {//west
 		m_throne->SetPos(m_bossPos.x + 33.35f, m_bossPos.y + 6.11f, m_bossPos.z);
@@ -267,16 +269,15 @@ void Gameplay::Initialize()
 		m_pSkeleBoss->SetThronePosition(m_throne->GetPos().x, m_throne->GetPos().y, m_throne->GetPos().z +1.f);
 		m_pSkeleBoss->GetEntity()->SetRot(0.f, 90.f, 0.f);
 		m_grave.GetEntity()->SetRot(0, -90, 0);
-		m_exit.GetEntity()->SetRot(-90, 0, 90);
+		m_exit.GetEntity()->SetRot(0, -90, 0);
 	}
 	//m_pSkeleBoss->SetThronePosition(m_throne->GetPos());
 	m_door.GetEntity()->SetPos(doorPos.x, 0.f, doorPos.z);
 	m_door.GetEntity()->MoveRelative(0.f, 0, 39.5f);
 	m_grave.GetEntity()->SetPos(0, -5, 0);
 
-	m_exit.GetEntity()->SetPos(doorPos.x, 3.f, doorPos.z);
-	m_exit.GetEntity()->MoveRelative(0, -20.5, 0);
-
+	m_exit.GetEntity()->SetPos(doorPos.x, 10.f, doorPos.z);
+	m_exit.GetEntity()->MoveRelative(0, 0, -21.5f);
 	//m_attack->SetParent(*m_player);
 
 	//printf("");
@@ -353,13 +354,13 @@ void Gameplay::Initialize()
 	m_Mat["InnerEdgeColor"] = Aen::Color::White;
 	m_Mat["OuterEdgeColor"] = Aen::Color::White;
 	m_Mat["GlowColor"] = Aen::Color::White;
-	m_Mat["OpacityStr"] = 0.001f;
+	m_Mat["OpacityStr"] = 1.f;
 
 	m_bill = &Aen::EntityHandler::CreateEntity();
 	m_bill->AddComponent<Aen::MeshInstance>();
 	m_bill->GetComponent<Aen::MeshInstance>().SetMesh("PLANE");
 	m_bill->GetComponent<Aen::MeshInstance>().SetMaterial("Bill");
-	m_bill->SetScale(0, 1, 0);
+	m_bill->SetScale(0, 0, 0);
 	m_bill->SetRenderLayer(2);
 
 	Aen::Input::ToggleRawMouse(true);
@@ -439,7 +440,7 @@ void Gameplay::Update(const float& deltaTime) {
 		mp_uiComp->SetDraw(false);
 		m_TransTimer += deltaTime * 0.5f;
 		m_Mat["OpacityStr"] = m_TransTimer;
-		m_bill->SetScale((10 / 1920.f) * screenSize.x, 1, (5 / 1024.f) * screenSize.y);
+		m_bill->SetScale((10 / 1920.f) * screenSize.x, (5 / 1024.f) * screenSize.y, 0);
 
 		if (m_TransTimer > 1.5f) {
 			SetWin(true);
@@ -458,8 +459,8 @@ void Gameplay::Update(const float& deltaTime) {
 
 	m_player.Update(m_enemyQueue, deltaTime);
 	m_bill->SetPos(m_player.GetCamera()->GetPos());
-	m_bill->SetRot(-m_player.GetCamera()->GetRot().x -90.f, m_player.GetCamera()->GetRot().y + 180.f, 0);
-	m_bill->MoveRelative(0, -2, 0);
+	m_bill->SetRot(m_player.GetCamera()->GetRot().x, m_player.GetCamera()->GetRot().y, 0);
+	m_bill->MoveRelative(0, 0, -2);
 
 	m_chest.Update(deltaTime, m_player.GetEntity());
 	m_door.Update(deltaTime, m_player.GetEntity());
@@ -490,7 +491,7 @@ void Gameplay::Update(const float& deltaTime) {
 		else if(m_door.GetType() == Type::Closed && m_door.GetNear()) {
 			m_door.SetType(Type::Opening);
 		}
-		else if (m_grave.GetNear() && m_grave.GetType() == Type::Open && m_player.GetBossesAlive() <= 0.f) {
+		else if (m_grave.GetNear() && m_grave.GetType() == Type::Closed) {
 			m_exit.SetType(Type::Opening);
 		}
 	}
@@ -499,7 +500,7 @@ void Gameplay::Update(const float& deltaTime) {
 
 		m_grave.SetType(Type::Closed);
 		m_grave.GetEntity()->SetPos(m_door.GetEntity()->GetPos());
-		m_grave.GetEntity()->MoveRelative(0.f, 0, -15.f);
+		m_grave.GetEntity()->MoveRelative(0.f, 0, -55.f);
 	}
 
 	for (auto& i : m_enemyQueue) {
